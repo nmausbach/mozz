68472604f3b2677bafe6b2f2a94e2a53a30d2f34
nmausbach <56225438+nmausbach@users.noreply.github.com>
2019-10-06 12:01:08 -0500

    Add files via upload

---
+++ Invoice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Invoice
+{
+    class Invoice
+    {
+        public string partNumber;
+        public string partDescription;
+        public int quantity;
+        public decimal price;
+        public void SetNumber(string partNumber1)
+        {
+            partNumber = partNumber1;
+        }
+        public string GetNumber()
+        {
+            return partNumber;
+        }
+        public void SetDescription(string partDescription1)
+        {
+            partDescription = partDescription1;
+        }
+        public string GetDescription()
+        {
+            return partDescription;
+        }
+        public void SetQuantity(int partQuantity1)
+        {
+            quantity = partQuantity1;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+        public void SetPrice(decimal price1)
+        {
+            price = price1;
+        }
+        public decimal GetPrice()
+        {
+            return price;
+        }
+    }  
+}
