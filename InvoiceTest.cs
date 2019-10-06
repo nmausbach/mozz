a96f72487e23e34b07a56b4513c9af6708d6cdd6
nmausbach <56225438+nmausbach@users.noreply.github.com>
2019-10-06 11:59:47 -0500

    Add files via upload

---
+++ InvoiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InvoiceTest
+{
+    class InvoiceTest
+    {
+        static void Main()
+        {
+            Invoice invoice1 = new Invoice();
+
+            Console.WriteLine($"Part Number: {invoice1.partNumber}\nPart Description: {partDescription}\nQuantity {quantity}\n Price: {price}");
+        }
+    }
+}
